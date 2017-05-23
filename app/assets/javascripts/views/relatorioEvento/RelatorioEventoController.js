544ce976554ac6ee3934e6a4c53876f473e01216
Thays <tg.vidal0@gmail.com>
2017-05-23 16:16:21 -0300

    relatorio evento

--- app/assets/javascripts/views/relatorioEvento/RelatorioEventoController.js
+++ app/assets/javascripts/views/relatorioEvento/RelatorioEventoController.js
@@ -6,19 +6,41 @@
     RelatorioEventoController.$inject = ['RelatorioEventoService', 'sessao', '$Respostas', '$window'];
 
     function RelatorioEventoController(RelatorioEventosService, sessao, $Respostas, $window) {
-        var REvm = this;
-        REvm.busca;
-        REvm.buscar = buscar;
-        REvm.radio = "nome";
+        var vm = this;
+        vm.busca;
+        vm.botao = false;
+        vm.data = data;
+        vm.nomeEvento = nomeEvento;
+        vm.buscar = buscar;
+        vm.radio = "nome";
+        vm.mensagem;
 
-        function buscar(){
-            console.log("oi");
+        function RelatEvento() {
+                vm.botao = true;
+                RelatorioEventosService.gerarRelatN(vm.nomeEvento)
+                    .then(function(data){
+                        console.log(data);
+                        vm.mensagem = '';
+                        switch (data.erro){
+                            case '000':
+                                console.log(data.body);
+                                vm.mensagem = "Relatorio Gerado";
+                                $window.location.href = "#";
+                                break;
+                            case '301':
+                                vm.mensagem = 'Relatório Indisponivel';
+                            case '302':
+                                vm.mensagem = 'Relatório Inexistente';
+                            default:
+                                vm.mensagem = 'Erro: ' + $Repostas[data.erro];
+                        }
+                        vm.botal = false;
+                    });
+            }
         }
-
-        var init = function () {
-
+        function limpar() {
+            vm.botao = false;
+            vm.matricula = '';
         }
-
-        init();
     }
 })();
