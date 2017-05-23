724ca78e33211348cbf0683196a0194578344777
Thays <tg.vidal0@gmail.com>
2017-05-23 14:39:54 -0300

    editar user controller

--- app/assets/javascripts/views/editarUser/EditarUserController.js
+++ app/assets/javascripts/views/editarUser/EditarUserController.js
@@ -7,8 +7,38 @@
 
 	function EditarUserController(EditarUserService, $Respostas, $window) {
 		var vm = this;
-		
-		
+		vm.senha = "";
+		vm.mensagem;
+		vm.mac = "";
 
+		function editarUser(){
+			if(vm.form.$invalid){
+				alert("Preencha os campos corretamente");
+			}
+		} 
+		else {
+
+			EditarUserService.editarUser(vm.mac, vm.senha)
+				.then(function(data){
+					vm.mensagem = '';
+				switch(data.erro){
+					case "000":
+					console.log(data.body);
+					vm.mensagem = "Alteração concluída com sucesso!";
+					$window.location.href = "#";
+					break;
+				default:
+					vm.mensagem = "Erro: " + $Respostas[data.erro];
+					switch (data.erro){
+						case "321"
+						vm.mensagem = "";
+						break;
+						case "322"
+						vm.mensagem = "";
+						break;
+					}
+				}					
+		
+		}
 	}
-})
+})();
