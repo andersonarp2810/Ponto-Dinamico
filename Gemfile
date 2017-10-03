00f6673fa84c830e99fe0064926fd6d2124da2ab
Unknown <jophev3@hotmail.com>
2017-10-03 16:36:41 -0300

    config.serve_static_files = true

--- Gemfile
+++ Gemfile
@@ -5,7 +5,6 @@ git_source(:github) do |repo_name|
   "https://github.com/#{repo_name}.git"
 end
 
-gem 'rails_12factor', group: :production
 #gem angular
 gem 'angular-rails-templates'
 #gem rack-cors
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -85,6 +85,6 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
 
   #mais uma do rails = pegar no heroku
-  config.serve_static_assets = true
+  config.serve_static_files = true
   config.assets.digest = true
 end
