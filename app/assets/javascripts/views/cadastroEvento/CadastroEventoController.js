49746737192284cd2df043d70816ecd3e887d705
Anderson Rodrigues Palmeira <andersonarp2810@gmail.com>
2017-10-04 16:53:52 -0300

    mapinha daora

--- app/assets/javascripts/views/cadastroEvento/CadastroEventoController.js
+++ app/assets/javascripts/views/cadastroEvento/CadastroEventoController.js
@@ -139,45 +139,104 @@
             });
             // jquery
 
-            //mapa
-            var map;
-
-            function initialize() {
-                var myLatlng = new google.maps.LatLng(vm.latitude, vm.longitude);
-
-                var myOptions = {
-                    zoom: 20,
-                    center: myLatlng,
-                    mapTypeId: google.maps.MapTypeId.ROADMAP
-                }
-                map = new google.maps.Map(document.getElementById("map-canvas"), myOptions);
-
-                var marker = new google.maps.Marker({
-                    draggable: true,
-                    position: myLatlng,
-                    map: map,
-                    title: "Sua localização"
-                });
-
-                google.maps.event.addListener(map, 'click', function (event) {
-                    marker.setPosition(event.latLng);
-                    marker.setTitle("" + event.latLng);
-                    console.log(event.latLng.lat());
-                    console.log(event.latLng.lng());
-                    vm.latitude = event.latLng.lat();
-                    vm.longitude = event.latLng.lng();
-                    //alert(event.latLng);
-                });
-            }
-
-            google.maps.event.addDomListener(window, 'load', initialize);
-            //mapa
-
-
-
+            
+            google.maps.event.addDomListener(window, 'load', iniciarMapa);
         }
 
+        // mapa
+        function iniciarMapa() {
+
+            var myLatlng = new google.maps.LatLng(vm.latitude, vm.longitude);
+
+            var map = new google.maps.Map(document.getElementById('map'), {
+                zoom: 20,
+                center: myLatlng,
+                mapTypeId: 'roadmap'
+            });
+
+            var marker = new google.maps.Marker({
+                draggable: false,
+                position: myLatlng,
+                map: map,
+                title: "Sua localização"
+            });
+
+            google.maps.event.addListener(map, 'click', function (event) {
+                marker.setPosition(event.latLng);
+                marker.setTitle("" + event.latLng);
+                console.log(event.latLng.lat());
+                console.log(event.latLng.lng());
+                $scope.$apply(function () {
+                    vm.latitude = event.latLng.lat();
+                    vm.longitude = event.latLng.lng();
+                });
+                //alert(event.latLng);
+            });
+
+
+            // Create the search box and link it to the UI element.
+            var input = document.getElementById('pac-input');
+            var searchBox = new google.maps.places.SearchBox(input);
+            map.controls[google.maps.ControlPosition.TOP_LEFT].push(input);
+
+            // Bias the SearchBox results towards current map's viewport.
+            map.addListener('bounds_changed', function () {
+                searchBox.setBounds(map.getBounds());
+            });
+
+            var markers = [];
+            // Listen for the event fired when the user selects a prediction and retrieve
+            // more details for that place.
+            searchBox.addListener('places_changed', function () {
+                var places = searchBox.getPlaces();
+
+                if (places.length == 0) {
+                    return;
+                }
+
+                // Clear out the old markers.
+                markers.forEach(function (marker) {
+                    marker.setMap(null);
+                });
+                markers = [];
+
+                // For each place, get the icon, name and location.
+                var bounds = new google.maps.LatLngBounds();
+                places.forEach(function (place) {
+                    if (!place.geometry) {
+                        console.log("Returned place contains no geometry");
+                        return;
+                    }
+                    var icon = {
+                        url: place.icon,
+                        size: new google.maps.Size(71, 71),
+                        origin: new google.maps.Point(0, 0),
+                        anchor: new google.maps.Point(17, 34),
+                        scaledSize: new google.maps.Size(25, 25)
+                    };
+
+                    // Create a marker for each place.
+                    markers.push(new google.maps.Marker({
+                        map: map,
+                        icon: icon,
+                        title: place.name,
+                        position: place.geometry.location
+                    }));
+
+                    if (place.geometry.viewport) {
+                        // Only geocodes have viewport.
+                        bounds.union(place.geometry.viewport);
+                    } else {
+                        bounds.extend(place.geometry.location);
+                    }
+                });
+                map.fitBounds(bounds);
+            });
+        }
+        //mapa
+
         init();
+
 
 
 
