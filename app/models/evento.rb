ba73ff0d34b900362ec48f9b7e5d68232546c62d
Unknown <jophev3@hotmail.com>
2017-10-10 14:26:53 -0300

    ajuste no registrar ponto

--- app/models/evento.rb
+++ app/models/evento.rb
@@ -79,8 +79,8 @@ private
     #realizar verificação de coordenada
     def self.valida_coodernada(coordenada)
         #deve retornar true
-    distancia = 6371 * Math.acos(Math.cos(Math::PI * (90 - coordenada [:LatB]) / 180) * Math.cos((90 - coordenada [:LatA]) * Math::PI / 180) + Math.sin((90 - coordenada [:LatB]) * Math::PI / 180) * Math.sin((90 - coordenada [:LatA])*Math::PI/180)*Math.cos(( coordenada [:LngA] - coordenada [:LngB])*Math::PI / 180))
-    distancia < 0.5
+        distancia = 6371 * Math.acos(Math.cos(Math::PI * (90 - coordenada [:LatB]) / 180) * Math.cos((90 - coordenada [:LatA]) * Math::PI / 180) + Math.sin((90 - coordenada [:LatB]) * Math::PI / 180) * Math.sin((90 - coordenada [:LatA])*Math::PI/180)*Math.cos(( coordenada [:LngA] - coordenada [:LngB])*Math::PI / 180))
+        distancia < 0.5
     end
 
     def self.registrar_ponto
@@ -94,7 +94,12 @@ private
             if usuario_evento.last.data != data_atual
                 usuario_evento = UsuarioEvento.new
                 usuario_evento.data = data_atual
+                usuario_evento.evento_id = @evento.id
+                usuario_evento.usuario_id = @usuario_id
                 usuario_evento.save
+            else
+                usuario_evento = usuario_evento.last
+            end
 
             if usuario_evento.hora_inicio.nil?#verifica se o usuario ja fez o primeiro ponto
                 #ponto no inicio do evento
